5280ab138f2a953ff20d30f25cd694c0839b8378
Ahhgust <August.Woerner@unthsc.edu>
2019-11-25 16:27:02 -0600

    Added R

--- R/initMMDIT.R
+++ R/initMMDIT.R
@@ -6,19 +6,38 @@
 # And it loads it with the appropriate data
 
 # database dependencies
+
+library(devtools)
+
+# Note to self
+# Install bit, bit64 and blob FIRST!
+# the dependencies are not handled (correctly)...
+#devtools::install_github("Ahhgust/RSQLite", ref="devel", upgrade=TRUE)
+#devtools::install_github("Ahhgust/Haplotypical", ref="master", upgrade=TRUE)
+
+
+library(Rcpp)
+library(DBI)
 library(RSQLite)
 library(sqldf)
 library(XLConnect)
+library(Haplotypical)
+
 
 # quality of life dependencies
-suppressPackageStartupMessages( library(tidyverse) )
+suppressPackageStartupMessages( library(tibble) )
+suppressPackageStartupMessages( library(readr) )
+suppressPackageStartupMessages( library(dplyr) )
+suppressPackageStartupMessages( library(magrittr) )
+
+
 
 # for reading fasta files (reference genome)
 library(seqinr)
 
 # CONSTANTS
 # data directory
-dataDir <- 'data'
+dataDir <- 'mito_data'
 
 # final database filename
 dbName <- 'mmdit.sqlite3'
@@ -27,179 +46,327 @@ dbName <- 'mmdit.sqlite3'
 refGenome <- 'rCRS.fasta'
 
 precisionIDBed <- "PrecisionID_mtDNA_WG_targets.bed"
+default_kit <- "PrecisionID"
 
-sqliteURL <- 'https://www.sqlite.org/src/tarball/sqlite.zip.gz?r=release'
-
-editdistFile <- 'spellfix.c'
-
-
-# helper function. 
+# helper function.
 # Normally we send a query and then clear it. this wraps it up into 1 statement
 dbSendQueryAndClear <- function(db, query) {
-  dbClearResult( dbSendQuery(db, query) )
+  DBI::dbClearResult( DBI::dbSendQuery(db, query) )
 }
 
-# This function takes in a path (e.g., data/mmdit.sqlite3)
-# and creates a database with the requisite tables in it
-# if overwrite is FALSE, then it won't overwrite an existing database
-# in that path.
-# otherwise, it will clobber what was there (use overwrite=TRUE with caution!!)
+#' Creates (an empty) MMDIT database
+#'
+#' This *creates* an EMPTY MMDIT database
+#' @param path; a directory (e.g., data)
+#' @param a filename (e.g., mmdit.sqlite3)
+#' @param overwrite (if there's already a file with that name, should I overwrite it?)
+#' and creates a database with the requisite tables in it
+#' if overwrite is FALSE, then it won't overwrite an existing database
+#' in that path.
+#' otherwise, it will clobber what was there (use overwrite=TRUE with caution!!)
+#'
+#' @return the handle to the database (throughout all other documentation, this is db)
+#' @examples
+#' # Make a new MMDIT database...
+#'
+#' # db <- makeDB(dataDir, dbName, overwrite=TRUE )
+#' # add the reference genome (can be only 1)
+#' # loadReferenceGenome(db, paste(dataDir, refGenome, sep="/") )
+#'
 makeDB <- function(path, dbFilename, overwrite=FALSE) {
-  
+
+
+  if (! dir.exists(path) ) {
+    dir.create(path)
+    if (! dir.exists(path)) {
+      stop(
+        paste("Failed to create directory: ", path , "\nThis is probably a file permission issue...")
+      )
+      return(NULL)
+    }
+  }
+
   dbFile <- paste(path, dbFilename, sep="/")
-  
+
   if (file.exists(dbFile)) { #DB already exists
     if (overwrite==FALSE) {
       stop(
           paste("DB file " , dbFile, " already exists. Let's not overwrite things, shall we?")
           )
       return(NULL)
-    }   
+    }
   }
 
-  db <- dbConnect( RSQLite::SQLite(), dbFile, loadable.extensions = TRUE)
-  
+  db <- DBI::dbConnect( RSQLite::SQLite(), dbFile, loadable.extensions = TRUE)
+  RSQLite::initExtension(db)
+
+
   # create the database tables:
-  
+
   # information on the amplicons
-  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS amps") 
+  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS amps")
   dbSendQueryAndClear(db,
-              "CREATE TABLE amps 
-              (ampid INTEGER PRIMARY KEY ASC, 
-              kit TEXT NOT NULL, 
-              start INTEGER NOT NULL, 
-              stop INTEGER NOT NULL)" ) 
+              "CREATE TABLE amps
+              (ampid INTEGER PRIMARY KEY ASC,
+              kit TEXT NOT NULL,
+              start INTEGER NOT NULL,
+              stop INTEGER NOT NULL)" )
 
-  
-  # and the sample IDs (and their corresponding populations)
-  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS populations") 
+  # and the sample IDs (and their corresponding populations and information where they came from (provenance))
+  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS populations")
   dbSendQueryAndClear(db,
-              "CREATE TABLE populations 
-              (sampleid TEXT PRIMARY KEY, 
+              "CREATE TABLE populations
+              (sampleid TEXT PRIMARY KEY,
+              whence TEXT,
               pop TEXT NOT NULL)" )
-   
-  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS haplotypes")
-  dbSendQueryAndClear(db,
-              "CREATE TABLE haplotypes
-              (hapid INTEGER PRIMARY KEY ASC, 
-               ampid INTEGER NOT NULL,
-               sampleid TEXT NOT NULL,
-               haplotype TEXT NOT NULL, 
-               haplotype_count INTEGER DEFAULT 1,
-                 FOREIGN KEY (ampid) REFERENCES amps(ampid),
-                 FOREIGN KEY (sampleid) REFERENCES populations(sampleid))" )
 
-  #TODO: THIS IS WRONG!
-  # information on the amplicon dropout
-  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS amp_dropout")
+
+  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS full_mito_diffseqs")
   dbSendQueryAndClear(db,
-              "CREATE TABLE amp_dropout 
-              (ampid INTEGER NOT NULL,
-              dropout_prob REAL NOT NULL, 
+            "CREATE TABLE full_mito_diffseqs
+              (genomeid INTEGER NOT NULL,
               sampleid TEXT NOT NULL,
-                 FOREIGN KEY (ampid) REFERENCES amps(ampid),
+                 position INTEGER NOT NULL,
+                 event TEXT NOT NULL,
+                 basecall TEXT NOT NULL,
                  FOREIGN KEY (sampleid) REFERENCES populations(sampleid),
-                 PRIMARY KEY (ampid, sampleid)
+                 PRIMARY KEY(genomeid)
               )" )
- 
-  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS haplogroups")
+
+
+  # a trivial table of just the reference genome
+  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS mtgenome")
   dbSendQueryAndClear(db,
-              "CREATE TABLE haplogroups 
-              (sampleid TEXT NOT NULL,
-                 haplogroup TEXT NOT NULL,
-                 FOREIGN KEY (sampleid) REFERENCES populations(sampleid),
-                 PRIMARY KEY (sampleid)
-              )" )
-  
-  # the individuals that we have full mitochondrial genomes for
-  # provenance must describe where the data came from
-  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS fullMtgenomes") 
-  dbSendQueryAndClear(db, 
-                      "CREATE TABLE fullMtgenomes
-                      (sampleid TEXT NOT NULL,
-                       provenance TEXT NOT NULL,
-                      FOREIGN KEY (sampleid) REFERENCES populations(sampleid),
-                      PRIMARY KEY (sampleid)
-                      )" )
-  
-  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS fullMtgenomesDiffseqs") 
-  dbSendQueryAndClear(db, 
-            "CREATE TABLE fullMtgenomesDiffseqs
-              (sampleid TEXT NOT NULL,
-                 diffPosition INTEGER NOT NULL,  
-                 diffEvent TEXT NOT NULL,
-                 diffBasecall TEXT NOT NULL,
-                 rowid INTEGER NOT NULL,
-                 FOREIGN KEY (sampleid) REFERENCES populations(sampleid),
-                 FOREIGN KEY (sampleid) REFERENCES fullMtgenomes(sampleid),
-                 PRIMARY KEY (rowid)
-              )" )
-  
-   
+                      "CREATE TABLE mtgenome
+              (mitoid INTEGER PRIMARY KEY ASC,
+                sequence TEXT,
+                seqlen INTEGER NOT NULL)")
+
+
+
 
   return(db)
 }
 
-# call this first.
-# this makes a table of just the reference sequence
-# this is hard-coded to the rCRS sequence
-# though this can be undone (doing so would require completely redoing the database)
-loadReferenceGenome <- function(db, fastaFile) {
-  # a trivial table of just the reference genome
-  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS mtgenome")
+#' Takes editdist3 distance into the unit-edit distance
+#'
+#' @param db the database (DBI object)
+#'
+#' the table that lets you specify the snp/indel costs in the editdist3 function
+#' as per:
+#' https://www.sqlite.org/spellfix1.html#the_editdist3_function
+initEditDists <- function(db) {
+  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS editcost")
   dbSendQueryAndClear(db,
-              "CREATE TABLE mtgenome
-              (mitoid INTEGER PRIMARY KEY ASC 
-                sequence TEXT,
-              seqlen INTEGER NOT NULL)")
-   
+                    "CREATE TABLE editcost
+                      (iLang INT,
+                      cFrom TEXT,
+                      cTo   TEXT,
+                      iCost INT)")
+
+  # makes this into a true unit editdist (all costs are 1)
+  tib <- tibble::tribble(
+    ~iLang, ~cFrom, ~cTo, ~iCost,
+       0,     '',    '?',  1,
+       0,     '?',    '',  1,
+       0,     '?',  '?',   1)
+
+
+  DBI::dbWriteTable(db,
+    "editcost",
+    tib, append=TRUE)
+}
+#' Use this to add the rCRS sequence to MMDIT
+#' It adds the record to the table mtgenome
+#' this table has *1* row in it. The reference genome.
+#' If you call this function multiple times, the last time
+#' will be the record that "sticks"
+#'
+#' Technically any genome would work here, BUT
+#' the difference encodings MUST be with respect to this reference genome!!
+#' i.e., in all likelihood this should *ONLY* be the rCRS sequence.
+#'
+#' @param db (database handle)
+#' @param fastafile (must be both the name of the file and the path to open it. This is passed directly to seqinr::read.fasta
+#'
+#' @examples
+#'
+#' # loadReferenceGenome(db, paste(dataDir, refGenome, sep="/") )
+#'
+loadReferenceGenome <- function(db, fastaFile) {
+
    fa <- seqinr::read.fasta(fastaFile, as.string=TRUE, seqtype="DNA", seqonly = TRUE)
    len <- nchar(fa)
    dbSendQueryAndClear(db,
                  sprintf(
-                   "INSERT INTO mtgenome (sequence, seqlen) VALUES
-                    (%d, \"%s\", %d)", 
+                   "INSERT INTO mtgenome (mitoid, sequence, seqlen) VALUES
+                    (%d, \"%s\", %d)",
                     1, fa[[1]], len[[1]])
                )
    return(db)
 }
 
-# this loads in amplicon data
-# the data are assumed to be in bed format. Since this is all one genome
-# I just use columns 2 and 3 from it to give me coordinates of the PCR products
-# (sans primers)
-# If the alignments were to a modified reference (duplicating bits to accommodate circular alignment)
-# this routine converts those back to the original units.
-# thus the last amplicon may "wrap around" (eg., 16541-80 is a valid locus)
-loadAmpData <- function(db, ampFile, sep="\t", kitName, append=TRUE) {
+#' Creates temp table of haplotypes
+#'
+#' this creates a temporary table called haplotypes
+#' The haplotypes table converts the difference encodings into strings (after applying the mask)
+#'
+#' #' @param db (database handle)
+makeHaplotypeTable <- function(db) {
+  dbSendQueryAndClear(db, "DROP TABLE IF EXISTS haplotypes")
+
+  dbSendQueryAndClear(db,
+                      "CREATE TEMP TABLE haplotypes
+              (hapid INTEGER NOT NULL,
+               ampid INTEGER NOT NULL,
+               sampleid TEXT NOT NULL,
+               haplotype TEXT NOT NULL,
+               haplotype_count INTEGER DEFAULT 1,
+                 FOREIGN KEY (ampid) REFERENCES amps(ampid),
+                 FOREIGN KEY (sampleid) REFERENCES populations(sampleid),
+                 PRIMARY KEY(hapid)
+               )" )
+  return(db)
+}
+
+#' this loads in amplicon data (i.e., the coordinates of the amps)
+#' into the MITO db.
+#' If the alignments were to a modified reference (duplicating bits to accommodate circular alignment)
+#' this routine converts those back to the original units.
+#' thus the last amplicon may "wrap around" (eg., 16541-80 is a valid locus)
+#'
+#' @param db (the database connection)
+#' @param ampfile (a BED file of amplicons; columns 2 and 3 are used only)
+#' @param kitname (the NAME of the kit that this corresponds to (e.g., PrecisionID) )
+#' @param sep (\"\\t\" for a tab-separated file. Anything else is a little wacky... no longer a bed file)
+#' @param append (whether or not the amplicons are APPENDED to the list of current amplicons; set to FALSE to overwrite)
+#'
+#' @return the database handle (not necessary to keep this)
+#'
+#' @examples
+#' # One way to add another kit:
+#'
+#' # loadAmpData(db, "data/Mybedfile.bed", kit="SomeOtherKit", append=TRUE)
+#'
+#'
+loadAmpData <- function(db, ampFile,  kitName, sep="\t", append=TRUE) {
    amps <- suppressMessages( readr::read_delim(ampFile, delim=sep) )
-   
-   genomeLength <- dbGetQuery(db, "SELECT seqlen FROM mtgenome LIMIT 1")[[1]]
+
+   overwrite<-FALSE
+   if (append == FALSE) {
+     overwrite<-TRUE
+   }
+
+   if (! overwrite) {
+     redundantCount <-
+       DBI::dbGetQuery(db, sprintf(
+       "SELECT count(kit) FROM amps WHERE kit == '%s'", kitName))
+
+    if (redundantCount[[1]] > 0) {
+        stop(
+          sprintf("Kit %s is already in the database...\nYou can't add it twice!", kitName)
+        )
+      return(db)
+    }
+   }
+   genomeLength <- DBI::dbGetQuery(db, "SELECT seqlen FROM mtgenome LIMIT 1")[[1]]
    if (max(amps[,3]) > genomeLength) { # this can happen. We assume it's wrap-around coordinates
       amps[,3] <- amps[,3] %% genomeLength
    }
-   amps.df <- tibble(kit=kitName, start=pull(amps,2), stop=pull(amps,3)) # coerce into a  tibble
-   dbWriteTable(db,
-                "amps", amps.df, append=append, overwrite=FALSE)
+   amps.df <- tibble::tibble(kit=kitName, start=dplyr::pull(amps,2), stop=dplyr::pull(amps,3)) # coerce into a  tibble
+   DBI::dbWriteTable(db,
+                "amps", amps.df, append=append, overwrite=overwrite)
    return(db)
 }
 
 
+#' Amplicon sequences of reference
+#'
+#' Generates the amplicon sequences
+#' based on the reference sequence.
+#' For use with MMDIT only!
+#'
+#' @param db the database connection
+#' @param kit the kit name (must be a valid KIT in the amps table)
+#' @return returns data frame with 4 columns: the amp ID, the start coordinate (0-based), the stop coordinate (1-based), and the Seq(uence) of the rcrs for the amp
+#'
+#' @example
+#' # amps <- getRefAmpSeqs(db, kit="PrecisionID")
+#'
+getRefAmpSeqs <- function(db, kit=default_kit) {
+  loc <- DBI::dbGetQuery(db,
+                    sprintf(
+                    "SELECT ampid, start, stop
+                    FROM
+                     amps
+                    WHERE
+                    kit= '%s'", kit) )
 
+  if (nrow(loc) == 0) {
+     stop(
+       paste(
+       "No data for kit ", kit , " was found...")
+     )
+  }
+  # get the mitochondrial genome (concatenated with itself)
+  # the self-concatentation allows for the easy lookup of amplicons that span the
+  # first and last bases of the linearized mitochondrial genome.
+  mtgenomeSeq <- stringr::str_dup(
+    DBI::dbGetQuery(db,
+                            "SELECT sequence FROM mtgenome LIMIT 1")[[1]],
+    2)
 
+  mtgenomeLen <- DBI::dbGetQuery(db,
+                            "SELECT seqlen FROM mtgenome LIMIT 1")[[1]]
 
+  # recall substr using 1-based indexing
+  # and the coordinates are bed format (0-based start, 1-based stop)
+  loc$Seq <-
+      stringr::str_sub(mtgenomeSeq,
+                       loc$start + 1,
+                       ifelse( loc$start < loc$stop,
+                         loc$stop,
+                         loc$stop + mtgenomeLen))
 
+  loc
+}
+
+#' Trivial testing to see if the editdist3 function works...
+#' Takes no arguments; returns a data frame (if editdist3 works!)...
+#' If this *doesn't* work you need to make sure you're
+#' using the development version of rsqlite as downloaded from my github at:
+#'
+#' devtools::install_github("Ahhgust/RSQLite", ref="devel", upgrade=TRUE)
+#'
+testEditdist <- function() {
+  db <- RSQLite::datasetsDb()
+  RSQLite::initExtension(db)
+  # 100 == insertion, 150 is used for substitution
+  DBI::dbGetQuery(db,"SELECT *, editdist3(row_names, 'Merc 230') AS editdist
+             FROM mtcars WHERE editdist3(row_names, 'Merc 230') < 251") -> tib
+  DBI::dbDisconnect(db)
+  return(tib)
+}
+
+getSeqdiffCodes <- function() {
+  c(X=1, I=2,D=3)
+}
+
+initDB <- function() {
 # makes the empty database
-db <- makeDB(dataDir, dbName, overwrite=TRUE )
+  db <- makeDB(dataDir, dbName, overwrite=TRUE )
 # add the reference genome (can be only 1)
-loadReferenceGenome(db, paste(dataDir, refGenome, sep="/") )
+  loadReferenceGenome(db, paste(dataDir, refGenome, sep="/") )
 # add amplicons for the precision ID panel
-loadAmpData(db, paste(dataDir, precisionIDBed, sep="/"), "\t", "PrecisionID" )
+  loadAmpData(db, paste(dataDir, precisionIDBed, sep="/"), kitName=default_kit, sep="\t" )
 
+# make the edit distance a true editdist
+  initEditDists(db)
 
-           
-
+  refAmps <- getRefAmpSeqs(db)
+  return(db)
+}
 # close the connection...
-dbDisconnect(db)
+#dbDisconnect(db)
 
 
---
+++ R/utpalFunctions.R
@@ -0,0 +1,180 @@
+#load packages
+suppressPackageStartupMessages(library(tidyr))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(stringr))
+suppressPackageStartupMessages(library(magrittr))
+suppressPackageStartupMessages(library(tibble))
+
+#' This function takes three vectors Position, Allele and Type from the SNPs
+#' and returns a vector of variants (e.g. "73G", "73+G", 73-" )
+
+#' @param Pos A vector of genomic positions for the SNPs (type numeric)
+#' @param Allele A vector of nucleotide bases present in the SNPs (character strings). Deletions are represented by ""
+#' @param Type A vector of the type of mutation represented by the SNP. Possible values include "Substitution", "Insertion"
+#' and "Deletion" (charachter strings)
+#'
+#' @examples
+#' # Snp2variant("73", "G", "Substitution")
+#' # Snp2variant(c("73", "95", "146"), c("G", "", "C"), c("Substitution", "Deletion", "Insertion"))
+#'
+Snp2variant<-function(Pos, Allele, Type){
+  tib<-dplyr::tibble(Pos=as.integer(Pos), Allele=Allele, Type=Type)
+  Variant<-dplyr::case_when( # create and keep only one vector - Variant
+    Type == "Substitution" ~ paste(Pos, Allele, sep=""), # if the Type is Substitution, concatenate Pos and Allele
+    Type == "Insertion" ~ paste(Pos, Allele, sep = "+"), # if the Type is Insertion, concatenate Pos and Allele
+    #seperated by +
+    Type == "Deletion" ~ paste(Pos, "-", sep = ""), # if the Type is Deletion, concatenate Pos and -
+    TRUE               ~ "?"
+    )
+}
+
+
+#' Makes an "empop" dataframe
+#'
+#'
+#' This function takes two vectors SampleID and Variant
+#' and returns a tibble in the empop format.
+#' Each row in the tibble will correspond to 1 individual (SampleID)
+#' and their empop string (tab separated) will be the value in the second column
+#'
+#' @importFrom magrittr %>%
+#' @param SampleID A vector of sample IDs (character strings)
+#' @param Variant A vector of variants (character strings). See Snp2variant for description
+#'
+#' @examples
+#'
+#' # write_mbop("NA12871", c("73G", "95-"))
+#' # write_mbop(c("NA12871", "NA12871", "NA12872"), c("73G", "95-", "73G"))
+#'
+write_mbop<-function(SampleID, Variant){
+  tib<-tibble::tibble(SampleID=SampleID, Variant=Variant)
+  Empop<-tib%>%dplyr::group_by(SampleID)%>%dplyr::summarise(empopstring = paste(unique(Variant),collapse = "\t"))
+}
+
+#' reads empop
+
+#' This function takes an empop file containing at least four (mandatory) columns
+#' (refer to https://empop.online/downloads for the emp file format)
+#' and returns a tibble with two columns - SampleID and Variant
+#'
+#' @importFrom magrittr %>%
+#' @param EMPOP An EMPOP file (tab seperated)
+#' @param s a numeric argument that tells the function how many rows of data to skip while reading in EMPOP file
+#' If no value is provided, the first line is ommited by default
+#'
+#' @examples
+#' #   Empop2variant("EMPOP.emp")
+#' #   Empop2variant("EMPOP.emp", s = 3)
+Empop2variant<-function(EMPOP, s = 1){
+  EMPOP<- readr::read_delim(EMPOP, delim = "\t", skip = s, col_names = FALSE) %>% # read the empop files in
+    dplyr::rename(SampleID = X1, Haplogroup = X2, Frequencies = X3)
+  LongF<- tidyr::gather(EMPOP,"Key","Variant",-SampleID, -Haplogroup, -Frequencies, factor_key = FALSE)
+  if(ncol(LongF)==3) {
+    dplyr::mutate(LongF,Variant = NA) %>% dplyr::select(-Haplogroup, -Frequencies)-> LongF
+  }
+  LongF%>%dplyr::select(SampleID, Variant) -> LongF
+}
+
+#' Converts 2 snp...
+#'
+#' This function takes a vector of variants (e.g. "73G", "73+G", 73-" ) and returns a tibble
+#' with 3 columns : Pos, Allele and Type that contain information on the genomic position,
+#' nucleotide base and type of mutation (i.e. "Substitution", "Insertion" and "Deletion") respectively for each variant
+#'
+#' @importFrom magrittr %>%
+#'
+#' @param Variant A vector of variants (character strings). See Snp2variant for description
+#'
+#' @examples
+#' # Variant2snp("73G")
+#' # Variant2snp(c("73G", "95-", "146+C"))
+#'
+Variant2snp<-function(Variant){
+  df<-(stringr::str_replace(Variant,"\\+", "\\."))# replace all occurences of "+" with a "."
+  df1<-stringr::str_replace_all(df, "^[^0-9]","")%>% tibble::enframe()# replace any non numeric element at the start of the string with an empty string and make the result into a dataframe
+  df2<-dplyr::rename(df1,Var = "value")%>% dplyr::select(-(1))# rename column to "Var" to be able to manipulate it downstream
+  df3<-df2%>%
+    tidyr::extract(Var, into=c("neg", "Pos", "Ins", "Allele"), "^([A-Z-])?(\\d+)(\\.\\d*)?([^.]*)") %>% # split Var into four columns
+    dplyr::mutate(Len=ifelse(Ins==".", 1, as.integer(sub(".", "", Ins))), # create a column "Len"
+           Allele = dplyr::case_when(                                     # populate the columen "Allele"...
+             base::grepl("^del|-", Allele, ignore.case = T)~ "",         # with an empty string if vector contains "del" or "-"
+             is.na(Len) | Len==1 ~ Allele,                         # with existing string if the Len is 1 or NA
+             !is.na(Len) | Len>1 ~ str_dup(Allele, Len),           # with existing string mutliplied by the number in column Len
+             TRUE ~ "?"                                            # with a "?" if none of these cases occur
+           ),
+           Type = dplyr::case_when(                                                         # create a column "Type" and populate it...
+             base::grepl("\\.\\d|\\.", Ins)~ "Insertion",                                  # with "Insertion" if the column "Ins" contains a "." or digit
+             base::grepl("^[ACGTRYSWKMBDHVN]$", Allele, ignore.case = TRUE)~"Substitution",# with "Substitution" if the column "Allele" contains a nucleotide or the IUPAC code
+             Allele == "" ~ "Deletion",                                              # with "Deletion" if the column "Allele" has an empty string
+             TRUE ~ "?"                                                              # with a "?" if none of these cases occur
+           )
+    ) %>%
+    dplyr::select(-(neg), -(Ins), -(Len))                                # drop all unnecessary columns
+}
+
+#' Makes a giant empop file...
+#'
+#' This function takes a lookup-table file and a path to EMPOP files
+#' and returns a bigger EMPOP file (i.e. all EMPOP files in the path bound together)
+#' Note: This function is not for general use. It was constructed specifically
+#' to construct haplotypes from HmtDB (https://www.hmtdb.uniba.it/) using custom up-stream processing.
+#'
+#' @importFrom magrittr %>%
+#'
+#'
+#' @param LUT A lookup-table file (tab seperated)
+#' @param Pa The path to folder where EMPOP files are stored (charachter string)
+#'
+#'@examples
+#'
+#' # Hmtdb2Empop(LUT, Pa = R.home())
+#'
+#'
+Hmtdb2Empop<-function(LUT,Pa) {
+
+  #function to add the amp column to the longfile2 format
+  add_amp<-function(e) {
+    tib<-Empop2variant(e) # run Empop2Variant
+    tib$Amp<-stringr::str_extract(e, "[\\d]+") # add a column called "Amp"
+    #and populate with only the digits extracted from filenames
+    return(tib) #return the dataframe
+  }
+
+  l<-base::list.files(Pa, pattern = ".emp")# read in the name of empop files in current folder
+  empop<-base::lapply(l, add_amp)%>% #run through the list and for each file in the list add an "Amp" column
+    dplyr::bind_rows()%>% tidyr::drop_na()#  join all elements of the list into a dataframe and drop missing values
+
+
+  t<-readr::read_tsv(LUT, col_names = FALSE )# read lookup table into R
+  t%>%dplyr::select_if(~!(all(is.na(.)) | all(. == "")))-> t # remove empty columns
+  t<-dplyr::rename(t, "Amp" = X5)#change the column name to "Amp" to be able to join with empop file later
+  t$Amp<-as.character(t$Amp)# coerce number to charachter to be able to join with empop file later
+
+  empop1<- dplyr::inner_join(t, empop, by = "Amp" ) # join longfile2 with lookuptable
+  dplyr::anti_join(t, empop1, by = "X6")%>%dplyr::pull(X6) -> ids # pull out all individuals that have all amps same as rCRS
+  empop2<-dplyr::mutate(empop1, Sort =(stringr::str_extract(Variant, "\\d+"))) # create a column "Sort" and populate it with just the numbers
+  #from the Variant column
+  empop2$Sort<-as.numeric(empop2$Sort)#convert "Sort" into type numeric
+  empop3<-empop2%>%dplyr::arrange(Sort) # sort "Sort" in ascending order
+  empop4<-dplyr::select(empop3, -(X1:X4), -(Amp), -(SampleID), -(Sort)) # delete unwanted columns
+  #empop5<-rename(empop4, SampleID = ncol(empop4))# rename the last column to "SampleID"
+  empop5<-dplyr::rename(empop4, SampleID = X6)# rename the last column to "SampleID"
+  Empop<-empop5%>%dplyr::group_by(SampleID)%>%dplyr::summarise(empopstring =base::paste(unique(Variant),collapse = "\t"))#go from long to wide format
+  Empop<-dplyr::select(Empop, SampleID, dplyr::everything())%>% # move "SampleID" to the beginging of dataframe
+    dplyr::mutate(Haplogroup = "", Frequencies = "") %>% # add the two other needed columns to dataframe
+    dplyr::select(SampleID, Haplogroup, Frequencies, dplyr::everything())# change order of the columns
+  Empop <-tibble::add_row(Empop, .before = 1)%>% # add the first row for title of study and author details
+    tibble::add_row(.before = 2) %>% # add the second row for geo background
+    tibble::add_row(.before = 3, SampleID = "!#" ) # add third row for sequence range
+
+  #create a tibble of indvidials with same amps as rCRS
+  tibble::tibble(SampleID = unique(ids),
+         Haplogroup = "",
+         Frequencies = "",
+         empopstring = "")-> foo
+
+  Empop <- dplyr::bind_rows(Empop, foo)# bind the above tibble to the final EMPOP file
+
+ utils::write.table(Empop, "Empop.tsv", quote = FALSE, sep = "\t", col.names = FALSE, row.names = FALSE, na="") # write tab-delimited file to file
+#without column or row names
+}
